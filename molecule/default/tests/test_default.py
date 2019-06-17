6dd98e46bab32033995a1f0059741ea7557822a8
Lucas Maurice <lucas.maurice@outlook.com>
2019-06-17 16:32:20 -0400

    Add molecule tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_user_creation(host):
+    os = host.system_info.distribution
+    user = host.user("bob")
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert user.exists
+    assert user.name == 'bob'
+    assert user.gecos == 'Bob'
+    assert user.home == '/home/bob'
+    assert user.shell == '/bin/bash'
+
+    with host.sudo(user.name):
+        groups = host.check_output("groups")
+
+    if os == 'debian':
+        assert 'sudo' in groups
+    elif os == 'redhat':
+        assert 'wheel' in groups
+
+
+def test_user_remove(host):
+    user = host.user('alice')
+    assert not user.exists
